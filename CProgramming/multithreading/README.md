05d49318239cbbae2da1c86d4ff647d9a15739ef
K-ojo <gideonad4@gmail.com>
2025-03-29 23:31:14 +0000

    added

---
+++ CProgramming/multithreading/README.md
@@ -0,0 +1,9 @@
+# ROAD MAP
+
+## 1️⃣ Understanding the Fundamental
+## 2️⃣ Understanding the Fundamental
+## 3️⃣ Understanding the Fundamental
+## 4️⃣ Understanding the Fundamental
+## 5️⃣ Understanding the Fundamental
+## 6️⃣ Understanding the Fundamental
+## 7️⃣ Understanding the Fundamental
--- CProgramming/multithreading/main.c
+++ CProgramming/multithreading/main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-void *thread_func(){
-    printf("Hello from thread%ld\n", pthread_self());
+uint64_t counter = 0;
+void *increment(){
+    for (int i = 0; i < 10000000000; i++){
+        counter++;
+    }
     return NULL;
 }
 
 int main(){
-    pthread_t thread;
-    pthread_create(&thread, NULL, thread_func, NULL);
-    pthread_join(thread, NULL);
+
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, increment, NULL);
+    pthread_create(&t2, NULL, increment, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
     return (0);
 }
