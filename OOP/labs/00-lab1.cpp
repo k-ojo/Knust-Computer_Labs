b50690ac9122012edf442c28136534da2ba7f97d
kojo <gideonad4@gmail.com>
2025-06-30 12:24:16 +0000

    added files

---
+++ OOP/labs/00-lab1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // Print a message to the console
+    std::cout << "Hello, World!" << std::endl;  //1.
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
